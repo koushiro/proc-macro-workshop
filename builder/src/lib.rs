1b7b733931aaf4150da8697aa53cf8244ebcef00
koushiro <koushiro.cqx@gmail.com>
2019-08-16 17:58:00 +0800

    Pass 02-create-builder
    Signed-off-by: koushiro <koushiro.cqx@gmail.com>

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,10 +1,39 @@
 extern crate proc_macro;
 
-use proc_macro::TokenStream;
-use syn::{parse_macro_input, DeriveInput};
+use proc_macro2::Span;
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput, Ident};
 
 #[proc_macro_derive(Builder)]
-pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = parse_macro_input!(input as DeriveInput);
-    proc_macro::TokenStream::new()
+pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    // Parse the input tokens into a syntax tree.
+    let input = parse_macro_input!(input as DeriveInput);
+
+    // Used in the quasi-quotation below as `#name`.
+    let name = input.ident;
+
+    let builder_name = Ident::new(&format!("{}Builder", name), Span::call_site());
+
+    let output = quote! {
+        impl #name {
+            pub fn builder() -> #builder_name {
+                #builder_name {
+                    executable: None,
+                    args: None,
+                    env: None,
+                    current_dir: None,
+                }
+            }
+        }
+
+        pub struct #builder_name {
+            executable: Option<String>,
+            args: Option<Vec<String>>,
+            env: Option<Vec<String>>,
+            current_dir: Option<String>,
+        }
+    };
+
+    // Hand the output tokens back to the compiler.
+    proc_macro::TokenStream::from(output)
 }
