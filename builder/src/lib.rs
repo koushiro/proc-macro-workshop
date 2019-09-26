e9e55acbb60b1a99dc2da1297111ed4afacf1aae
koushiro <koushiro.cqx@gmail.com>
2019-09-26 17:23:29 +0800

    Refactor
    Signed-off-by: koushiro <koushiro.cqx@gmail.com>

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -2,12 +2,10 @@ extern crate proc_macro;
 #[macro_use]
 extern crate quote;
 
-use syn::parse_macro_input;
-
-#[proc_macro_derive(Builder)]
+#[proc_macro_derive(Builder, attributes(builder))]
 pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     // Parse the input tokens into a syntax tree.
-    let input = parse_macro_input!(input as syn::DeriveInput);
+    let input = syn::parse_macro_input!(input as syn::DeriveInput);
 
     // Construct builder pattern for structure.
     let output = builder_for_struct(input);
@@ -17,109 +15,134 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
 }
 
 fn builder_for_struct(ast: syn::DeriveInput) -> proc_macro2::TokenStream {
-    let vis = ast.vis;
-    let name = ast.ident;
+    let ident = ast.ident;
+    let builder = format_ident!("{}Builder", ident);
+
+    let (impl_generics, ty_generics, where_clause) = ast.generics.split_for_impl();
+
     let fields = match ast.data {
         syn::Data::Struct(data) => data.fields,
-        _ => unreachable!(),
+        _ => panic!("#[derive(Builder)] is only defined for structs"),
     };
 
-    let builder_name = format_ident!("{}Builder", name);
+    match fields {
+        syn::Fields::Named(_) => {},
+        _ => panic!("#[derive(Builder)] is not defined for Unnamed structs and Uint structs"),
+    }
 
-    let empty_fields = fields
-        .iter()
-        .map(|field| {
-            let ident = &field.ident;
-            quote!{ #ident: None }
-        });
-
-    let construct_fields = fields
-        .iter()
-        .map(|field| {
-            let ident = &field.ident;
-            let ty = &field.ty;
-            if is_optional_type(ty) {
-                quote!{ #ident: #ty }
-            } else {
-                quote!{ #ident: Option<#ty> }
-            }
-        });
-
-    let set_fields = fields
-        .iter()
-        .map(|field| {
-            let ident = &field.ident;
-            let ty = &field.ty;
-            if is_optional_type(ty) {
-                quote! { #ident: self.#ident.clone() }
-            } else {
-                quote! { #ident: self.#ident.clone().ok_or(concat!("Field [", stringify!(#ident), "] is missing"))? }
-            }
-        });
-
-    let setters = fields.iter().map(|field| {
-        let vis = &field.vis;
-        let ident = &field.ident;
-        let ty = &field.ty;
-        if let Some(inner_ty) = inner_type("Option", ty) {
-            quote! {
-                #vis fn #ident(&mut self, #ident: #inner_ty) -> &mut Self {
-                    self.#ident = Some(#ident);
-                    self
-                }
-            }
-        } else {
-            quote! {
-                #vis fn #ident(&mut self, #ident: #ty) -> &mut Self {
-                    self.#ident = Some(#ident);
-                    self
-                }
-            }
-        }
-    });
+    let builder_default_fields = builder_default_fields(&fields);
+    let builder_fields = builder_fields(&fields);
+    let builder_setters = builder_setters(&fields);
+    let build_set_fields = build_set_fields(&fields);
 
     quote! {
-        impl #name {
-            #vis fn builder() -> #builder_name {
-                #builder_name {
-                    #(#empty_fields,)*
+        impl #impl_generics #ident #ty_generics #where_clause {
+            pub fn builder() -> #builder {
+                #builder {
+                    #( #builder_default_fields )*
                 }
             }
         }
 
-        #vis struct #builder_name {
-            #(#construct_fields,)*
+        pub struct #builder #ty_generics {
+            #( #builder_fields )*
         }
 
-        impl #builder_name {
-            #(#setters)*
+        impl #impl_generics #builder #ty_generics #where_clause {
+            #( #builder_setters )*
 
-            #vis fn build(&mut self) -> Result<#name, Box<dyn ::std::error::Error>> {
-                Ok(#name {
-                    #(#set_fields,)*
+            pub fn build(&mut self) -> Result<#ident, Box<dyn std::error::Error>> {
+                Ok(#ident {
+                    #( #build_set_fields )*
                 })
             }
         }
     }
 }
 
-fn inner_type<'a>(string: &str, ty: &'a syn::Type) -> Option<&'a syn::Type> {
-    if let syn::Type::Path(syn::TypePath { path, .. }) = ty {
-        if path.segments.len() != 1 || path.segments[0].ident != string {
-            return None;
-        }
-        if let syn::PathArguments::AngleBracketed(ref inner) = path.segments[0].arguments {
-            if inner.args.len() != 1 {
-                return None;
-            }
-            if let syn::GenericArgument::Type(ref ty) = inner.args.first().unwrap() {
-                return Some(ty);
-            }
-        }
-    }
-    None
+fn builder_default_fields(
+    fields: &syn::Fields,
+) -> impl Iterator<Item = proc_macro2::TokenStream> + '_ {
+    fields.iter().map(|field| {
+        let ident = &field.ident;
+            quote! { #ident: None, }
+    })
 }
 
-fn is_optional_type(ty: &syn::Type) -> bool {
-    inner_type("Option", ty).is_some()
+fn builder_fields(fields: &syn::Fields) -> impl Iterator<Item = proc_macro2::TokenStream> + '_ {
+    fields.iter().map(|field| {
+        let ident = &field.ident;
+        let ty = &field.ty;
+        if field_is(field, "Option") {
+            quote! { #ident: #ty, }
+        } else {
+            quote! { #ident: Option<#ty>, }
+        }
+    })
+}
+
+fn builder_setters(fields: &syn::Fields) -> impl Iterator<Item = proc_macro2::TokenStream> + '_ {
+    fields.iter().map(|field| {
+        let ident = &field.ident;
+        let ty = &field.ty;
+        if field_is(field, "Option") {
+            let inner_ty = generic_inner_type(field);
+            quote! {
+                pub fn #ident(&mut self, #ident: #inner_ty) -> &mut Self {
+                    self.#ident = Some(#ident);
+                    self
+                }
+            }
+        } else {
+            quote! {
+                pub fn #ident(&mut self, #ident: #ty) -> &mut Self {
+                    self.#ident = Some(#ident);
+                    self
+                }
+            }
+        }
+    })
+}
+
+fn build_set_fields(fields: &syn::Fields) -> impl Iterator<Item = proc_macro2::TokenStream> + '_ {
+    fields
+        .iter()
+        .map(|field| {
+            let ident = &field.ident;
+            if field_is(field, "Option") {
+                quote! { #ident: self.#ident.take(), }
+            } else {
+                quote! { #ident: self.#ident.take().ok_or(concat!("Field [", stringify!(#ident), "] is missing"))?, }
+            }
+        })
+}
+
+fn field_is(field: &syn::Field, ty: &str) -> bool {
+    if let syn::Type::Path(ref path) = field.ty {
+        path.path
+            .segments
+            .first()
+            .map(|seg| seg.ident == ty)
+            .unwrap_or(false)
+    } else {
+        unimplemented!()
+    }
+}
+
+fn generic_inner_type(field: &syn::Field) -> &syn::Type {
+    match field.ty {
+        syn::Type::Path(ref path) => path
+            .path
+            .segments
+            .first()
+            .map(|seg| match seg.arguments {
+                syn::PathArguments::AngleBracketed(ref args) => match args.args.first().unwrap() {
+                    syn::GenericArgument::Type(ty) => ty,
+                    _ => unimplemented!(),
+                },
+                _ => unimplemented!(),
+            })
+            .unwrap(),
+        _ => unimplemented!(),
+    }
 }
